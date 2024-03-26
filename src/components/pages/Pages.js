f9dd07b54aa2e403a4dcc6028d81faffa6bad6d0
inanct <inanc.tekinoktay@gmail.com>
2024-03-26 23:56:08 +0300

    header

---
+++ src/components/pages/Pages.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../common/header/Header'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+
+const Pages = () => {
+  return (
+    <>
+    <Router>
+      <Header/>
+      <Routes>
+
+      </Routes>
+    </Router>
+        
+    </>
+  )
+}
+
+export default Pages
